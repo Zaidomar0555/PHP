a1a76444e0c358e9aba2e1765a0557c28e02686e
Zaidomar0555 <zaidomar0555@gmail.com>
2021-01-11 22:25:31 +0300

    still working on the main.js

--- PHPLessonOne/js/main.js
+++ PHPLessonOne/js/main.js
@@ -1,26 +1,26 @@
 // this is for client side
 $(document).ready(function() {
   var country = document.querySelector("#country").options;
-  //var countries = document.getElementById("myCountry");
+  var countries = document.getElementById("myCountry");
   var cities = document.querySelector("#city").options;
   var getInfo;
   //var counter = cities.length;
-  var pickUSA = country[6].value;
-  //console.log(pickUSA);
-  console.log(cities.value);
+  var pickUSA = country[4].value;
+   //console.log(pickUSA);
+  // console.log(cities);
   // $("#country").options.value
 
 
-  function selectCity(country.value){
-    for(var i = 0; i < cities.length; i++){
-
-       if(cities[i].value == country.value){
-         console.log(cities[i]);
-         cities
-         continue;
-       }
-    }
+  for(var i = 0; i < cities.length; i++){
+     if(cities[i].value == pickUSA){
+       console.log(cities[i]);
+      getInfo = cities[i];
+      countries.textContent = getInfo;
+       continue;
+     }
   }
+
+ //countries.textContent = getInfo;
 
 
   });
